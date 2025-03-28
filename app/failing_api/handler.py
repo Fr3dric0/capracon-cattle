d000fdffa4cb0acd1a2d1818f1e270715066215e
Fredrik F. Lindhagen <fredrik.forde.lindhagen@dnb.no>
2025-03-28 10:12:56 +0100

    Use html instead of json in resposne

--- app/failing_api/handler.py
+++ app/failing_api/handler.py
@@ -1,5 +1,19 @@
-import json
 import os
+
+def html_frame(body: str) -> str:
+    return f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Capracon cattle</title>
+    </head>
+    <body>
+        {body.strip()}
+    </body>
+    </html>
+    """.strip()
 
 def lambda_handler(event, context):
     should_die = os.environ.get('SHOULD_DIE', 'false') == 'true'
@@ -8,17 +22,27 @@ def lambda_handler(event, context):
     if should_die:
         return {
             'statusCode': 500,
-            'body': json.dumps({
-                'error': 'Failed to start',
-                'region': region,
-            })
+            'headers': {
+                'Content-Type': 'text/html',
+            },
+            'body': html_frame(f"""
+            <header>
+            <h1>I have problems in {region}</h1>
+            </header>
+            <div style="width:100%;height:0;padding-bottom:55%;position:relative;"><iframe src="https://giphy.com/embed/2UCt7zbmsLoCXybx6t" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/this-is-fine-2UCt7zbmsLoCXybx6t">via GIPHY</a></p>
+            """)
         }
 
     return {
         'statusCode': 200,
-        'body': json.dumps({
-            'message': 'Hello world',
-            'region': region,
-        })
+        'headers': {
+            'Content-Type': 'text/html',
+        },
+        'body': html_frame(f"""
+        <header>
+        <h1>All good in {region}</h1>
+        </header>
+        <div style="width:100%;height:0;padding-bottom:83%;position:relative;"><iframe src="https://giphy.com/embed/zhRA0okWxTGiu78uSk" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/theoffice-the-office-tv-episode-801-zhRA0okWxTGiu78uSk">via GIPHY</a></p>
+        """)
     }
 
